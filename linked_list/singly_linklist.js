743c14ac0df3213b2081b5b0c81f64e759baeaa9
kazimurtuza <kazimurtuza11@gmail.com>
2022-12-07 02:30:52 +0600

    singly linked link push

--- linked_list/singly_linklist.js
+++ linked_list/singly_linklist.js
@@ -1,1 +1,37 @@
-let a = 10;
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      //   {val:'kazi',next:{val:'kazi',next:new obj()}}
+      this.tail = newNode;
+      // last added new object
+    }
+    this.length++;
+
+    return this;
+  }
+}
+
+let link = new SinglyLinkedList();
+let link1 = link.push("kazi");
+let link2 = link.push("kazi 1");
+let link3 = link.push("kazi 2");
+
+console.log(link3);
