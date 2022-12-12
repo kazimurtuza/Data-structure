afef73d299a471d85e5147c56bac08ecf7f9f209
kazimurtuza <kazimurtuza11@gmail.com>
2022-12-13 00:44:36 +0600

    singly linklist done

--- linked_list/singly_linklist.js
+++ linked_list/singly_linklist.js
@@ -54,20 +54,117 @@ class SinglyLinkedList {
       this.tail = null;
     }
   }
+  unshift(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index > this.length) {
+      return null;
+    } else {
+      var i = 0;
+      var current = this.head;
+      while (i < index) {
+        current = current.next;
+        i++;
+      }
+      return current;
+    }
+  }
+  set(index, val) {
+    var target = this.get(index);
+    if (target) {
+      target.val = val;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, val) {
+    var newNode = new Node(val);
+    if (index < 0 || index > this.length) {
+      return false;
+    }
+    if (index === 0) {
+      this.unshift(val);
+    } else if (index === this.length) {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    } else {
+      let target = this.get(index);
+      if (target) {
+        let previousNode = this.get(index - 1);
+        newNode.next = target;
+        previousNode.next = newNode;
+      }
+    }
+    this.length++;
+  }
+  remove(index) {
+    if (index < 0 || index > this.length) {
+      return false;
+    } else if (index === 0) {
+      this.shift();
+    } else if (index === this.length) {
+      this.pop();
+    } else {
+      let target = this.get(index);
+      if (target) {
+        let firstpart = this.get(index - 1);
+        let secondpart = this.get(index + 1);
+        firstpart.next = secondpart;
+      }
+    }
+    this.length--;
+  }
+
+  revers() {
+    var node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    var next;
+    var previous = null;
+
+    var i = 1;
+    for (i; i < this.length; i++) {
+      next = node.next;
+      node.next = previous;
+      previous = node;
+      node = next;
+    }
+    return this;
+  }
+
   node() {
     return this;
   }
 }
 
 let link = new SinglyLinkedList();
-let link1 = link.push("kazi");
-let link2 = link.push("kazi 1");
-let link3 = link.push("kazi 2");
-let pop1 = link.pop();
+link.push("name 1");
+link.push("name 2");
+link.push("name 3");
+link.push("name 4");
+link.push("name 5");
 
-console.log(pop1);
-console.log("---------------");
-console.log(link.pop());
-link.shift();
+// console.log(link.node());
+// console.log(link.get(4));
+// console.log(link.set(10, 7777));
 
-console.log(link.node());
+// console.log(link.node(0));
+// link.insert(4, 30);
+// console.log(link.node());
+// console.log(link.get(4));
+// link.remove(0);
+// console.log(link.node());
+
+link.revers();
